555700603422cd760a86e8983302fa590be2fbb7
Todor Chenkov <joto_to@mail.bg>
2019-01-11 12:30:59 +0200

    02. Insert Data in Tables

---
+++ 01. Data Definition And Data Types - Lab/02. Insert Data in Tables.sql
@@ -0,0 +1,11 @@
+# 02. Insert Data in Tables
+#
+# Insert data into "employees" table from "gamebar" database.
+
+USE gamebar;
+
+INSERT INTO employees (first_name, last_name)
+VALUES
+	('ivan','ivanov'),
+    ('sybi', 'syvev'),
+    ('spas','spasov');
