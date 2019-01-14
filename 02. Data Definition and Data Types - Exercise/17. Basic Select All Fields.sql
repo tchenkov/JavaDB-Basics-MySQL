f47faa1f85b5cc6230d0056bebd74932b064fc36
Todor Chenkov <joto_to@mail.bg>
2019-01-14 16:20:23 +0200

    17. Basic Select All Fields

---
+++ 02. Data Definition and Data Types - Exercise/17. Basic Select All Fields.sql
@@ -0,0 +1,15 @@
+#	17. Basic Select All Fields
+#
+# Use the soft_uni database and first select all records from the towns,
+# then from departments and finally from employees table.
+
+USE soft_uni;
+
+SELECT *
+FROM towns;
+
+SELECT *
+FROM departments;
+
+SELECT *
+FROM employees;
