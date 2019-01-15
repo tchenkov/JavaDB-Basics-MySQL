174675a9ad50114925664926511080ccd2547472
Todor Chenkov <joto_to@mail.bg>
2019-01-15 12:50:36 +0200

    01. Find All Information About Departments

---
+++ 04. Basic CRUD - Exercise/01. Find All Information About Departments.sql
@@ -0,0 +1,10 @@
+#	01. Find All Information About Departments
+#
+# Write a SQL query to find all available information about the
+# departments. Sort the information by id.
+
+USE soft_uni;
+
+SELECT *
+FROM departments
+ORDER BY department_id;
