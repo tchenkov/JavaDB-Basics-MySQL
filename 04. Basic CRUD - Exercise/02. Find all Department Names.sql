56c3ef2f5c7b88784f5d3b5d961235191af001fc
Todor Chenkov <joto_to@mail.bg>
2019-01-15 12:51:56 +0200

    02. Find all Department Names

---
+++ 04. Basic CRUD - Exercise/02. Find all Department Names.sql
@@ -0,0 +1,9 @@
+#	02. Find all Department Names
+#
+# Write SQL query to find all department names. Sort the information by id.
+
+USE soft_uni;
+
+SELECT name
+FROM departments
+ORDER BY department_id;
